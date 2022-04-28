6f7ae8179443c34fff11ef28e662dd5a056c81b7
Zevety <90498783+Zevety@users.noreply.github.com>
2022-04-28 18:20:55 +0300

    Add files via upload

---
+++ assets/images/index.js
@@ -0,0 +1,7 @@
+const images2 = {
+  cat: require("../../cat.jpg"),
+  cat2: require("../../cat2.jpg"),
+  cat3: require("../../cat4.jpg"),
+  cat4: require("../../cat3.jpg")
+}
+export default images2;
